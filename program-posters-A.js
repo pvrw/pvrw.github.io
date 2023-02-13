3f8796a5b197a57bd923b62b605df31960c5a046
Silverman, Timothy <timothy.silverman@nrel.gov>
2023-02-13 14:20:28 -0700

    indents in js files

--- program-posters-A.js
+++ program-posters-A.js
@@ -1,1 +1,268 @@
-var program = [{"number": 1, "authors": "Walid Arsalane, Ethan Young, Chris Ivanov, Scott Dana, Mike Deceglie", "affiliation": "National Renewable Energy Laboratory", "title": "A simulation framework for managing wind-driven loading on PV systems", "duramat": true}, {"number": 4, "authors": "Mike Kempe*, Silvana Ovaitt, Martin Springer", "affiliation": "National Renewable Energy Laboratory", "title": "Industry Facing PV Degradation Prediction Tool and Degradation Database to Enable a 50 Year Life Module", "duramat": true}, {"number": 7, "authors": "Benjamin Pierce*, Jennifer Braid", "affiliation": "Sandia National Laboratories", "title": "A generative model for synthetic electroluminescence images", "duramat": true}, {"number": 10, "authors": "Dana B. Kern*, Steve Johnston, Helio Moutinho, Jenya Meydbray, Chun-Sheng Jiang, Dirk Jordan", "affiliation": "National Renewable Energy Laboratory", "title": "Solder Pad Degradation in Field-Weathered Silicon Photovoltaic Modules", "duramat": true}, {"number": 13, "authors": "Joseph Karas*, Ingrid Repins", "affiliation": "National Renewable Energy Laboratory", "title": "An open-source python library for modeling LETID and LID in silicon solar cells and wafers", "duramat": true}, {"number": 16, "authors": "Peter Hacke*, Nick Bosco, James Hartley, David Miller", "affiliation": "National Renewable Energy Laboratory", "title": "Characterization of interconnects", "duramat": true}, {"number": 19, "authors": "Will Hobbs", "affiliation": "Southern Company", "title": "EL imaging with the Raspberry Pi  Camera Module 3", "duramat": true}, {"number": 22, "authors": "Teresa Barnes*, Laura T. Schelhas*, Clifford W. Hansen, Anubhav Jain, Michael Owen-Bellini, David Miller, Bruce King", "affiliation": "National Renewable Energy Laboratory", "title": "DuraMAT: Durable Modules Materials Consortium", "duramat": true}, {"number": 25, "authors": "Robert White*, Nalinrat Guba, Ashlee Vise, Nicholas Wunder, and Rachel Hurst", "affiliation": "NREL", "title": "DuraMAT Data Hub", "duramat": true}, {"number": 28, "authors": "Jennifer L. Braid*, Phillip L. Reu, James Y. Hartley", "affiliation": "Sandia National Laboratories", "title": "Digital Image Correlation Analysis of Hail Impacts on PV Modules", "duramat": true}, {"number": 31, "authors": "Xin Chen*, Baojie Li*, Todd Karin, Anubhav Jain", "affiliation": "Lawrence Berkeley National Lab", "title": "Assessing PV degradation through analysis of operation and bill-of-materials data", "duramat": true}, {"number": 34, "authors": "Martin Springer*, Laura Spinella, Timothy J. Silverman, Matthew Young, and Ingrid Repins", "affiliation": "National Renewable Energy Laboratory", "title": "Quantifying cell fragment movement of fractures c-Si mini-modules", "duramat": true}, {"number": 37, "authors": "Junki Joe*, Michael Owen-Bellini, Timothy Silverman, Nick Bosco", "affiliation": "NREL", "title": "Residual stress limits gridline bridging of cracked solar cells", "duramat": true}, {"number": 40, "authors": "Michael Owen-Bellini, James Y. Hartley, Anubhav Jain, Dirk C. Jordan, Laura T. Schelhas, Teresa M. Barnes", "affiliation": "NREL", "title": "Highlights from 5 Years of DuraMAT", "duramat": true}, {"number": 43, "authors": "Tim Townsend, Jon Previtali", "affiliation": "VDE", "title": "A Fresh Dusting: Current Applications of the Townsend Snow Model", "duramat": true}, {"number": 46, "authors": "Silvana Ovaitt, Chris Deline, Dirk Jordan, Elizabeth Palmiotti, Dana Kern, Steve Johnston, Byron MacDanold", "affiliation": "NREL", "title": "A study in bifacial field degradation", "duramat": true}, {"number": 49, "authors": "So\u0148a Uli\u010dn\u00e1*, Dennice M. Roberts, Michael Owen-Bellini, Paul Ndione, Helio Moutinho, Kent Terwilliger, Steve Johnston, Byron McDanold, Timothy J. Silverman, Laura Schelhas, and Dana B. Kern", "affiliation": "National Renewable Energy Laboratory", "title": "Bifacial PV Module Packaging Designs: Outdoor Aging vs Accelerated Stress Testing", "duramat": true}, {"number": 52, "authors": "Bruce H. King*, Laurie Burnham, Tap Lolla, Wayne Li, Vignesh Ramasamy, Andy Walker, Jal Desai, Michael Woodhouse", "affiliation": "Sandia National Laboratories", "title": "PV connector reliability and field forensics", "duramat": true}, {"number": 55, "authors": "Daniel Fregosi*, Rabin Dhakal, Devin Widrick", "affiliation": "Electric Power Research Institute", "title": "Performance loss rate comparison between satellite and ground based sensors for normalization", "duramat": true}, {"number": 58, "authors": "David H Meakin", "affiliation": "GAF Energy", "title": "Qualification of a BIPV Nailable Solar Shingle", "duramat": true}, {"number": 61, "authors": "Farhan Rahman and James Y. Hartley*", "affiliation": "Sandia National Laboratories", "title": "Software Program to Convert Between SIERRA And COMSOL Simulation Codes for Modeling Capability Sharing", "duramat": true}, {"number": 64, "authors": "George Kelly", "affiliation": "Sunset Technology Inc.", "title": "IEC TC 82 status", "duramat": false}, {"number": 67, "authors": "James Willett*, Beth Copanas", "affiliation": "RES", "title": "Is capacity testing a reliable metric for single-axis tracking (sat) pv plant performance?", "duramat": false}, {"number": 70, "authors": "John H Wohlgemuth*", "affiliation": "PowerMark Corp", "title": "PV standards activities of IEC", "duramat": false}, {"number": 73, "authors": "Rich Stromberg*", "affiliation": "Alaska Center for Energy and Power", "title": "Reuse of photovoltaic modules for social and ecological benefit", "duramat": false}, {"number": 76, "authors": "Daniel Herron*, Adam Shinn", "affiliation": "kWh Analytics", "title": "Solar generation index: data-driven performance trends", "duramat": false}, {"number": 79, "authors": "Rachel Woods-Robinson", "affiliation": "University of California Berkeley", "title": "The carbon cost of a PhD in photovoltaics", "duramat": false}, {"number": 82, "authors": "Ann Bradish*, Beth Copanas, James Willett", "affiliation": "Renewable Energy Systems", "title": "How does the density and distribution of meteorological sensors change the results of utility scale pv capacity testing?", "duramat": false}, {"number": 85, "authors": "Charity Faith Sotero", "affiliation": "kWh Analytics", "title": "Extracting Actionable Insight from O&M Logs Using NLP", "duramat": false}, {"number": 88, "authors": "Laura Clark*, Taylor Romshek, Stephen Ressler", "affiliation": "ICF", "title": "Module Recycling Options and Pitfalls", "duramat": false}, {"number": 91, "authors": "Christine Bordonaro*, Ph.D., Pierre Metaut, James Vance", "affiliation": "Clearway Energy", "title": "Geospatial impacts of energy on large utility scale solar power plants", "duramat": false}, {"number": 94, "authors": "Veronica Anderson*, Ben Browne, Jimmy Dunn, Adam Shinn", "affiliation": "kWh Analytics", "title": "Does the past predict the future?  Using historical life-to-date performance to predict future performance for operational systems", "duramat": false}, {"number": 97, "authors": "Abdullah F. Al-Aboosi*, Fadhil Y. Al-Aboosi, and Wei Zhan", "affiliation": "Texas A&M University", "title": "A Future Insight of High-Impact Potential of Solar Availability and Grid-Connected Photovoltaic System Use to decarbonize Energy Sector", "duramat": false}, {"number": 100, "authors": "Corson Teasley*, Scott Sheppard, Daniel Fregosi, Wayne Li", "affiliation": "Turbine Logic", "title": "Using current data to detect hardware faults at solar plants", "duramat": false}, {"number": 103, "authors": "Chris Jackson", "affiliation": "Luminant", "title": "Identifying Power Losses in Large Scale Solar Generation Sites", "duramat": false}, {"number": 106, "authors": "Bill Brooks", "affiliation": "Brooks Engineering", "title": "PV system fires: why they keep happening", "duramat": false}, {"number": 109, "authors": "Mary Werner", "affiliation": "National Renewable Energy Laboratory", "title": "NREL's Solar Strategy", "duramat": false}, {"number": 112, "authors": "Chris Deline and Bill Sekulic", "affiliation": "National Renewable Energy Laboratory", "title": "tbd", "duramat": false}]
+var program = [
+  {
+    "number": 1,
+    "authors": "Walid Arsalane, Ethan Young, Chris Ivanov, Scott Dana, Mike Deceglie",
+    "affiliation": "National Renewable Energy Laboratory",
+    "title": "A simulation framework for managing wind-driven loading on PV systems",
+    "duramat": true
+  },
+  {
+    "number": 4,
+    "authors": "Mike Kempe*, Silvana Ovaitt, Martin Springer",
+    "affiliation": "National Renewable Energy Laboratory",
+    "title": "Industry Facing PV Degradation Prediction Tool and Degradation Database to Enable a 50 Year Life Module",
+    "duramat": true
+  },
+  {
+    "number": 7,
+    "authors": "Benjamin Pierce*, Jennifer Braid",
+    "affiliation": "Sandia National Laboratories",
+    "title": "A generative model for synthetic electroluminescence images",
+    "duramat": true
+  },
+  {
+    "number": 10,
+    "authors": "Dana B. Kern*, Steve Johnston, Helio Moutinho, Jenya Meydbray, Chun-Sheng Jiang, Dirk Jordan",
+    "affiliation": "National Renewable Energy Laboratory",
+    "title": "Solder Pad Degradation in Field-Weathered Silicon Photovoltaic Modules",
+    "duramat": true
+  },
+  {
+    "number": 13,
+    "authors": "Joseph Karas*, Ingrid Repins",
+    "affiliation": "National Renewable Energy Laboratory",
+    "title": "An open-source python library for modeling LETID and LID in silicon solar cells and wafers",
+    "duramat": true
+  },
+  {
+    "number": 16,
+    "authors": "Peter Hacke*, Nick Bosco, James Hartley, David Miller",
+    "affiliation": "National Renewable Energy Laboratory",
+    "title": "Characterization of interconnects",
+    "duramat": true
+  },
+  {
+    "number": 19,
+    "authors": "Will Hobbs",
+    "affiliation": "Southern Company",
+    "title": "EL imaging with the Raspberry Pi  Camera Module 3",
+    "duramat": true
+  },
+  {
+    "number": 22,
+    "authors": "Teresa Barnes*, Laura T. Schelhas*, Clifford W. Hansen, Anubhav Jain, Michael Owen-Bellini, David Miller, Bruce King",
+    "affiliation": "National Renewable Energy Laboratory",
+    "title": "DuraMAT: Durable Modules Materials Consortium",
+    "duramat": true
+  },
+  {
+    "number": 25,
+    "authors": "Robert White*, Nalinrat Guba, Ashlee Vise, Nicholas Wunder, and Rachel Hurst",
+    "affiliation": "NREL",
+    "title": "DuraMAT Data Hub",
+    "duramat": true
+  },
+  {
+    "number": 28,
+    "authors": "Jennifer L. Braid*, Phillip L. Reu, James Y. Hartley",
+    "affiliation": "Sandia National Laboratories",
+    "title": "Digital Image Correlation Analysis of Hail Impacts on PV Modules",
+    "duramat": true
+  },
+  {
+    "number": 31,
+    "authors": "Xin Chen*, Baojie Li*, Todd Karin, Anubhav Jain",
+    "affiliation": "Lawrence Berkeley National Lab",
+    "title": "Assessing PV degradation through analysis of operation and bill-of-materials data",
+    "duramat": true
+  },
+  {
+    "number": 34,
+    "authors": "Martin Springer*, Laura Spinella, Timothy J. Silverman, Matthew Young, and Ingrid Repins",
+    "affiliation": "National Renewable Energy Laboratory",
+    "title": "Quantifying cell fragment movement of fractures c-Si mini-modules",
+    "duramat": true
+  },
+  {
+    "number": 37,
+    "authors": "Junki Joe*, Michael Owen-Bellini, Timothy Silverman, Nick Bosco",
+    "affiliation": "NREL",
+    "title": "Residual stress limits gridline bridging of cracked solar cells",
+    "duramat": true
+  },
+  {
+    "number": 40,
+    "authors": "Michael Owen-Bellini, James Y. Hartley, Anubhav Jain, Dirk C. Jordan, Laura T. Schelhas, Teresa M. Barnes",
+    "affiliation": "NREL",
+    "title": "Highlights from 5 Years of DuraMAT",
+    "duramat": true
+  },
+  {
+    "number": 43,
+    "authors": "Tim Townsend, Jon Previtali",
+    "affiliation": "VDE",
+    "title": "A Fresh Dusting: Current Applications of the Townsend Snow Model",
+    "duramat": true
+  },
+  {
+    "number": 46,
+    "authors": "Silvana Ovaitt, Chris Deline, Dirk Jordan, Elizabeth Palmiotti, Dana Kern, Steve Johnston, Byron MacDanold",
+    "affiliation": "NREL",
+    "title": "A study in bifacial field degradation",
+    "duramat": true
+  },
+  {
+    "number": 49,
+    "authors": "So\u0148a Uli\u010dn\u00e1*, Dennice M. Roberts, Michael Owen-Bellini, Paul Ndione, Helio Moutinho, Kent Terwilliger, Steve Johnston, Byron McDanold, Timothy J. Silverman, Laura Schelhas, and Dana B. Kern",
+    "affiliation": "National Renewable Energy Laboratory",
+    "title": "Bifacial PV Module Packaging Designs: Outdoor Aging vs Accelerated Stress Testing",
+    "duramat": true
+  },
+  {
+    "number": 52,
+    "authors": "Bruce H. King*, Laurie Burnham, Tap Lolla, Wayne Li, Vignesh Ramasamy, Andy Walker, Jal Desai, Michael Woodhouse",
+    "affiliation": "Sandia National Laboratories",
+    "title": "PV connector reliability and field forensics",
+    "duramat": true
+  },
+  {
+    "number": 55,
+    "authors": "Daniel Fregosi*, Rabin Dhakal, Devin Widrick",
+    "affiliation": "Electric Power Research Institute",
+    "title": "Performance loss rate comparison between satellite and ground based sensors for normalization",
+    "duramat": true
+  },
+  {
+    "number": 58,
+    "authors": "David H Meakin",
+    "affiliation": "GAF Energy",
+    "title": "Qualification of a BIPV Nailable Solar Shingle",
+    "duramat": true
+  },
+  {
+    "number": 61,
+    "authors": "Farhan Rahman and James Y. Hartley*",
+    "affiliation": "Sandia National Laboratories",
+    "title": "Software Program to Convert Between SIERRA And COMSOL Simulation Codes for Modeling Capability Sharing",
+    "duramat": true
+  },
+  {
+    "number": 64,
+    "authors": "George Kelly",
+    "affiliation": "Sunset Technology Inc.",
+    "title": "IEC TC 82 status",
+    "duramat": false
+  },
+  {
+    "number": 67,
+    "authors": "James Willett*, Beth Copanas",
+    "affiliation": "RES",
+    "title": "Is capacity testing a reliable metric for single-axis tracking (sat) pv plant performance?",
+    "duramat": false
+  },
+  {
+    "number": 70,
+    "authors": "John H Wohlgemuth*",
+    "affiliation": "PowerMark Corp",
+    "title": "PV standards activities of IEC",
+    "duramat": false
+  },
+  {
+    "number": 73,
+    "authors": "Rich Stromberg*",
+    "affiliation": "Alaska Center for Energy and Power",
+    "title": "Reuse of photovoltaic modules for social and ecological benefit",
+    "duramat": false
+  },
+  {
+    "number": 76,
+    "authors": "Daniel Herron*, Adam Shinn",
+    "affiliation": "kWh Analytics",
+    "title": "Solar generation index: data-driven performance trends",
+    "duramat": false
+  },
+  {
+    "number": 79,
+    "authors": "Rachel Woods-Robinson",
+    "affiliation": "University of California Berkeley",
+    "title": "The carbon cost of a PhD in photovoltaics",
+    "duramat": false
+  },
+  {
+    "number": 82,
+    "authors": "Ann Bradish*, Beth Copanas, James Willett",
+    "affiliation": "Renewable Energy Systems",
+    "title": "How does the density and distribution of meteorological sensors change the results of utility scale pv capacity testing?",
+    "duramat": false
+  },
+  {
+    "number": 85,
+    "authors": "Charity Faith Sotero",
+    "affiliation": "kWh Analytics",
+    "title": "Extracting Actionable Insight from O&M Logs Using NLP",
+    "duramat": false
+  },
+  {
+    "number": 88,
+    "authors": "Laura Clark*, Taylor Romshek, Stephen Ressler",
+    "affiliation": "ICF",
+    "title": "Module Recycling Options and Pitfalls",
+    "duramat": false
+  },
+  {
+    "number": 91,
+    "authors": "Christine Bordonaro*, Ph.D., Pierre Metaut, James Vance",
+    "affiliation": "Clearway Energy",
+    "title": "Geospatial impacts of energy on large utility scale solar power plants",
+    "duramat": false
+  },
+  {
+    "number": 94,
+    "authors": "Veronica Anderson*, Ben Browne, Jimmy Dunn, Adam Shinn",
+    "affiliation": "kWh Analytics",
+    "title": "Does the past predict the future?  Using historical life-to-date performance to predict future performance for operational systems",
+    "duramat": false
+  },
+  {
+    "number": 97,
+    "authors": "Abdullah F. Al-Aboosi*, Fadhil Y. Al-Aboosi, and Wei Zhan",
+    "affiliation": "Texas A&M University",
+    "title": "A Future Insight of High-Impact Potential of Solar Availability and Grid-Connected Photovoltaic System Use to decarbonize Energy Sector",
+    "duramat": false
+  },
+  {
+    "number": 100,
+    "authors": "Corson Teasley*, Scott Sheppard, Daniel Fregosi, Wayne Li",
+    "affiliation": "Turbine Logic",
+    "title": "Using current data to detect hardware faults at solar plants",
+    "duramat": false
+  },
+  {
+    "number": 103,
+    "authors": "Chris Jackson",
+    "affiliation": "Luminant",
+    "title": "Identifying Power Losses in Large Scale Solar Generation Sites",
+    "duramat": false
+  },
+  {
+    "number": 106,
+    "authors": "Bill Brooks",
+    "affiliation": "Brooks Engineering",
+    "title": "PV system fires: why they keep happening",
+    "duramat": false
+  },
+  {
+    "number": 109,
+    "authors": "Mary Werner",
+    "affiliation": "National Renewable Energy Laboratory",
+    "title": "NREL's Solar Strategy",
+    "duramat": false
+  },
+  {
+    "number": 112,
+    "authors": "Chris Deline and Bill Sekulic",
+    "affiliation": "National Renewable Energy Laboratory",
+    "title": "tbd",
+    "duramat": false
+  }
+]
